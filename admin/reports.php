b5c1100317b64dc029f3fe08402871d78a580213
MGChristian <132035945+MGChristian@users.noreply.github.com>
2025-05-03 23:40:01 +0800

    cleaned up admin instructors and reports page

--- admin/reports.php
+++ admin/reports.php
@@ -15,7 +15,6 @@ if (isset($_SESSION['error_login'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <?php include "./components/css.php" ?>
-    <link href="./css/admin-instructor.css" rel="stylesheet">
     <link href="./css/admin-modals.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.2.1/af-2.7.0/b-3.2.0/b-colvis-3.2.0/b-html5-3.2.0/b-print-3.2.0/cr-2.0.4/date-1.5.5/fc-5.0.4/fh-4.0.1/kt-2.12.1/r-3.0.3/rg-1.5.1/rr-1.5.0/sc-2.4.3/sb-1.8.1/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
