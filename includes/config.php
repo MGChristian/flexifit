e833a2c03432919bf8ee6edf6a8c9edce3bdb1d5
MGChristian <132035945+MGChristian@users.noreply.github.com>
2025-06-15 00:21:25 +0800

    updated

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,21 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 // $dbhostname = "localhost:3306";
 // $dbusername = "mgchristian_admin";
 
 // $database = "mgchristian_flexifit";
 
-$dbhostname = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$database = "flexifit";
+$dbhostname = $_ENV['DB_HOST'];
+$dbusername = $_ENV['DB_USER'];
+$dbpassword = $_ENV['DB_PASS'] ?? '';
+$database = $_ENV['DB_NAME'];
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
